54d5ff272703538933cfdc3cf28103d382c1861c
t-lohani <t.lohani@gmail.com>
2017-03-28 14:05:50 -0400

    Removing error handling

--- seawolf.py
+++ seawolf.py
@@ -8,23 +8,6 @@ from SeawolfExprParser import SeawolfExprParser
 from antlr4.error.ErrorListener import ErrorListener
 from MyVisitor import MyVisitor
 
-class MyErrorListener( ErrorListener ):
-
-    def __init__(self):
-        super(MyErrorListener, self).__init__()
-
-    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
-        raise Exception("Oh no!!")
-
-    def reportAmbiguity(self, recognizer, dfa, startIndex, stopIndex, exact, ambigAlts, configs):
-        raise Exception("Oh no!!")
-
-    def reportAttemptingFullContext(self, recognizer, dfa, startIndex, stopIndex, conflictingAlts, configs):
-        raise Exception("Oh no!!")
-
-    def reportContextSensitivity(self, recognizer, dfa, startIndex, stopIndex, prediction, configs):
-        raise Exception("Oh no!!")
-
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         input_stream = FileStream(sys.argv[1])
@@ -34,7 +17,6 @@ if __name__ == '__main__':
     lexer = SeawolfExprLexer(input_stream)
     token_stream = CommonTokenStream(lexer)
     parser = SeawolfExprParser(token_stream)
-    parser._listeners = [ MyErrorListener() ]
     tree = parser.prog()
 
     visitor = MyVisitor()
